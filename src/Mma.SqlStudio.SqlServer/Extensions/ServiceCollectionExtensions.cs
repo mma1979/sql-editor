44d6976791049458e53d259355f3e93048229c62
Mohammed Abdelhay <m.abdelhay.c@nhci.sa>
2026-04-28 23:33:13 +0300

    feat: implement initial UI structure for SQL Studio TestApp and SqlServer component grid

---
+++ src/Mma.SqlStudio.SqlServer/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Mma.SqlStudio.SqlServer.Models;
+using Mma.SqlStudio.SqlServer.Services;
+using System;
+
+namespace Mma.SqlStudio.SqlServer.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSqlStudio(this IServiceCollection services, Action<SqlStudioOptions> configureOptions)
+        {
+            if (configureOptions == null) throw new ArgumentNullException(nameof(configureOptions));
+
+            services.Configure(configureOptions);
+
+            services.AddScoped<EditorService>();
+            services.AddScoped<SchemaService>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddSqlStudio(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+
+            services.Configure<SqlStudioOptions>(configuration.GetSection("SqlStudio"));
+
+            services.AddScoped<EditorService>();
+            services.AddScoped<SchemaService>();
+
+            return services;
+        }
+    }
+}
