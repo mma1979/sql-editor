952ad001a67254d29f035f1458213e6c23baa245
Mohammed Abdelhay <m.abdelhay.c@nhci.sa>
2026-04-28 19:41:02 +0300

    feat: add connection dialog UI and supporting configuration models for multi-source connection management

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddScoped<sql_editor.Services.EditorService>();
+builder.Services.Configure<sql_editor.Models.SqlStudioOptions>(builder.Configuration.GetSection("SqlStudio"));
 
 var app = builder.Build();
 
